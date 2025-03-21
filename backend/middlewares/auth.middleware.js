312131e6d4157af6a2d4c8e4e2c533aeb7b1dfe6
Arj0n-G0d <gaurxarjun@gmail.com>
2025-03-21 21:12:51 +0530

    feat: Create volunteer model based on user model - Enhanced user.model.js with additional fields - Added phone, skills, location, availability fields - Created volunteer.model.js using user.model.js as a base

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,4 +1,4 @@
-import { User } from "../models/user.model.js";
+import { User } from "../models/volunteer.model.js";
 import jwt from "jsonwebtoken"
 export const isAuthenticated =async(req,res,next)=>{
      const {token}=req.cookies;
